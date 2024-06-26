457b3a20453329f7b6f88e5e41413296d91ed7c7
L0G1C06 <justgithub70@gmail.com>
2024-06-26 11:47:38 -0300

    feat: add plus study

--- api/utils.py
+++ api/utils.py
@@ -3,7 +3,6 @@ import playsound
 import os
 
 def ttsText2Audio(text, speaker_wav, output_path):
-    accept_tos = os.getenv('ACCEPT_TOS', '').lower() in ['true', '1']
     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
     tts.tts_to_file(text, speaker_wav=speaker_wav, language="pt", file_path=output_path)
 
